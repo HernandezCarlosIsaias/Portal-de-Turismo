1fa9321b2ba89b1759b97daa04a13e55c5fd85ce
HernandezCarlosIsaias <120617047+HernandezCarlosIsaias@users.noreply.github.com.>
2024-10-30 21:29:18 -0300

    aplicamos al admin la clase lugar turistico

--- gps_simulation/routes/admin.py
+++ gps_simulation/routes/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import City, Route
+from .models import City, Route, Lugar_Turistico
 
 admin.site.register(City)
 admin.site.register(Route)
+admin.site.register(Lugar_Turistico)
